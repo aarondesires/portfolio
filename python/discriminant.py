683740fdda5526b53f4a8f4e3bfc96e7d934ab52
Aaron <aaron.desires@gmail.com>
2024-09-03 16:25:27 -0400

    Create discriminant.py

---
+++ python/discriminant.py
@@ -0,0 +1,8 @@
+# Discriminant Formula
+print("Hello! This very simple python project is used to calculate the DISCRIMINANT")
+a = int(input("Enter A: "))
+b = int(input("Enter B: "))
+c = int(input("Enter C: "))
+
+ans = (b**2)-4*a*c
+print(ans)
