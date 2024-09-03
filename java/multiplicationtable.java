2e148965506b27e1961b7773714cb3bb1cad1cd9
Aaron <aaron.desires@gmail.com>
2024-09-03 15:42:55 -0400

    Create multiplicationtable.java

---
+++ java/multiplicationtable.java
@@ -0,0 +1,13 @@
+public class Extra26Nested {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\t\t\t<<Multiplication Table>>");
+		for (int i=1;i<=9;i++) {
+			for (int j=1;j<=9;j++) {
+				System.out.print(i+"*"+j+"="+(i*j)+"\t");
+			}
+			System.out.println();
+		}
+	}
+}
